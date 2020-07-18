89d65114c80d76d9a61ab014707d3be14c74e294
Hardeep Singh <expodevclub@gmail.com>
2020-07-19 04:41:17 +0530

    first

--- server.js
+++ server.js
@@ -5,6 +5,24 @@ const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 
 
 
 const app = express();
+const mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var db = 'mongodb+srv://expodev:expodev123@payflex.d7ybj.mongodb.net/sample_weatherdata?retryWrites=true&w=majority';
+mongoose.connect(db, {   
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+ }
+ ).then(console.log("connected"));
+
+
+ var schemaName = new Schema({
+  request: String,
+  time: Number
+}, {
+  collection: 'accounts'
+});
+
+var Model = mongoose.model('Model', schemaName);
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
@@ -14,6 +32,33 @@ app.use(bodyParser.json())
 app.get('/get', (req, res) => {
   res.send('Welcome Tp Express Api')
 
+})
+
+app.get('/dbs', (req, res) => {
+  res.send (
+    mongoose.connect(db, {   
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+     }
+     ).then(console.log("connected"))
+  )
+
+})
+app.get('/data', cors(), function(req, res) {
+  var query = req.params.query;
+
+  Model.find({
+      'request': query
+  }, function(err, result) {
+      if (err) throw err;
+      if (result) {
+          res.json(result)
+      } else {
+          res.send(JSON.stringify({
+              error : 'Error'
+          }))
+      }
+  })
 })
 
 app.get('/gets', (req, res) => {
